dff8b877412f06ee4b11920b3fbd093cded6667e
Alexander <alexandervvugt@gmail.com>
2020-03-30 19:29:32 +0200

    Query method and save operations movierepo

--- src/bioscoop_app/Repository/MovieRepository.cs
+++ src/bioscoop_app/Repository/MovieRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 using System.Linq;
 using bioscoop_app.Model;
@@ -11,13 +12,15 @@ namespace bioscoop_app.Repository
     public class MovieRepository
     {
         private const string FileName = "movies.json";
-        private Dictionary<int, Movie> Movies;
+        private Dictionary<int, Movie> Movies { get; }
+        private bool IsOpen { get; } = false;
 
         public MovieRepository()
         {
             Movies = JsonConvert.DeserializeObject<Dictionary<int, Movie>>(
                 File.ReadAllText(StorageService.GetDataSourcePath() + FileName)
             );
+            IsOpen = true;
         }
 
         public static void SetupDataSource()
@@ -34,23 +37,69 @@ namespace bioscoop_app.Repository
 
         public void AddMovie(Movie movie)
         {
-            try
+            if (!IsOpen)
+            {
+                throw new System.InvalidOperationException();
+            }
+            if(Movies.Count() == 0)
+            {
+                movie.id = 0;
+            } else
             {
                 movie.id = Movies.Keys.Max() + 1;
             }
-            catch (Exception e)
-            {
-                movie.id = 0;
-            }
-            
             this.Movies.Add(movie.id, movie);
-            
+        }
+        
+        public void Discard()
+        {
+            IsOpen = false;
+        }
+
+        public void SaveChangesAndDiscard()
+        {
+            SaveChanges();
+            Discard();
+        }
+
+        public void AddMovieAndWrite(Movie movie)
+        {
+            AddMovie(movie);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
             File.WriteAllText(
                 StorageService.GetDataSourcePath() + FileName,
                 JsonConvert.SerializeObject(Movies)
             );
-            
         }
-        
+
+        public List<Movie> Query(string title, string genre, double rating, int duration, int limit)
+        {
+            List<Movie> resultSet = new ArrayList<Movie>();
+            foreach(Movie movie in Movies)
+            {
+                if (title == null || movie.title.Equals(title))
+                {
+                    if (genre == null || movie.genre.Equals(genre))
+                    {
+                        if (rating == null || movie.rating == rating)
+                        {
+                            if (duration == null || movie.duration == duration)
+                            {
+                                resultSet.Add(movie);
+                                if (resultSet.Count() == limit)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return resultSet;
+        }
     }
 }
