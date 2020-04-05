74af7d4acd920bd6382da0d756d3e5f7da76cd65
Alexander <alexandervvugt@gmail.com>
2020-04-05 17:13:27 +0200

    Documented CustomObject

--- src/bioscoop_app/CustomObject.cs
+++ src/bioscoop_app/CustomObject.cs
@@ -6,8 +6,16 @@ using System.Text;
 
 namespace bioscoop_app
 {
+	/// <summary>
+	/// Abstract class that dynamically overrides object.Equals, object.GetHashCode, and the == and != operators.
+	/// </summary>
     public abstract class CustomObject
     {
+		/// <summary>
+		/// Method to test for equality between two objects.
+		/// </summary>
+		/// <param name="other">The object to compare to.</param>
+		/// <returns><code>true</code> iff <code>this</code> and <code>other</code> are equal and from the same Type.</returns>
 		public override bool Equals(object other)
 		{
 			if (other is null) return false;
@@ -28,6 +36,10 @@ namespace bioscoop_app
 			return true;
 		}
 
+		/// <summary>
+		/// Uses hashes and primes to generate a semi-unique hashcode for this object.
+		/// </summary>
+		/// <returns>Generated hashcode as an int.</returns>
 		public override int GetHashCode()
 		{
 			unchecked
@@ -43,6 +55,13 @@ namespace bioscoop_app
 			}
 		}
 
+		/// <summary>
+		/// Operator to test for equality between two objects.
+		/// Behaves similar to a.Equals(b), but is null-reference safe.
+		/// </summary>
+		/// <param name="a">object a</param>
+		/// <param name="b">object b</param>
+		/// <returns><code>true</code> iff <code>a</code> and <code>b</code> are equal and from the same Type.</returns>
 		public static bool operator ==(CustomObject a, CustomObject b)
 		{
 			if (ReferenceEquals(a, b)) return true;
@@ -50,6 +69,13 @@ namespace bioscoop_app
 			return a.Equals(b);
 		}
 
+		/// <summary>
+		/// Operator to test for inequality between two objects.
+		/// Behaves opposite to the == operator. Null-reference safe.
+		/// </summary>
+		/// <param name="a">object a</param>
+		/// <param name="b">object b</param>
+		/// <returns><code>false</code> iff <code>a</code> and <code>b</code> are equal and from the same Type.</returns>
 		public static bool operator !=(CustomObject a, CustomObject b)
 		{
 			return !(a == b);
