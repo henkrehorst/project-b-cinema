46e1fd51ff4560e0463be243c15d26f382b758e4
Henk Rehorst <henkrehorst@outlook.com>
2020-04-06 13:27:44 +0200

    ADDED file upload field in movie form

--- src/bioscoop_app/frontend/js/add_movie.js
+++ src/bioscoop_app/frontend/js/add_movie.js
@@ -20,22 +20,33 @@ function $(el = '') {
 }
 
 document.querySelector('button.add-movie').addEventListener('click', () => {
-    let data = {
-        'title': $('input[name="movie-title"]').value,
-        'duration': parseInt($('input[name="movie-duration"]').value),
-        'genre': $('input[name="movie-genre"]').value,
-        'rating': parseFloat($('input[name="movie-rating"]').value),
-    };
+    // convert image file to base 64 image
+    getBase64String($('input[name="movie-cover-image"]').files[0]).then(function (fileString) {
+        let data = {
+            'title': $('input[name="movie-title"]').value,
+            'duration': parseInt($('input[name="movie-duration"]').value),
+            'genre': $('input[name="movie-genre"]').value,
+            'rating': parseFloat($('input[name="movie-rating"]').value),
+            'cover_image': fileString
+        };
 
-    console.log(data);
+        console.log(data);
 
-    postMovie(data);
+        postMovie(data);
+    });
 });
 
 
 function getBase64String(file) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    
-    
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            resolve(reader.result);
+        };
+        reader.error = function (error) {
+            reject(error);
+        };
+    });
 }
+
