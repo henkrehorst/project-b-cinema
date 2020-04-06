619b90081d8efbdc42f7c29cb6d19a2d4ef18250
Henk Rehorst <henkrehorst@outlook.com>
2020-04-06 13:36:55 +0200

    Make StorageService.cs class static

--- src/bioscoop_app/Service/StorageService.cs
+++ src/bioscoop_app/Service/StorageService.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace bioscoop_app.Service
 {
-    public sealed class StorageService
+    public static class StorageService
     {
         public static void SetupStorageFiles()
         {
