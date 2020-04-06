9a1a9687588043daeac03f85f6390f08ced28973
Alexander <alexandervvugt@gmail.com>
2020-04-06 11:25:03 +0200

    DataType docstring

--- src/bioscoop_app/Model/DataType.cs
+++ src/bioscoop_app/Model/DataType.cs
@@ -6,8 +6,14 @@ using System.Linq;
 
 namespace bioscoop_app.Model
 {
+    /// <summary>
+    /// Abstract class that is used for polymorphism and to store the id of Types that are in the database.
+    /// </summary>
     public abstract class DataType : CustomObject
     {
+        /// <summary>
+        /// The id by which you can fetch the instance from the database.
+        /// </summary>
         public int id;
 	}
 }
