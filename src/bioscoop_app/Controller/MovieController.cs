3a890bce13772065fb69a03a527fbf432485b606
ercu5314 <ercument-58@outlook.com>
2020-04-07 12:16:01 +0200

    Update moviecontroller
    Errormessages gedaan samen met Danny.

--- src/bioscoop_app/Controller/MovieController.cs
+++ src/bioscoop_app/Controller/MovieController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using bioscoop_app.Model;
 using Chromely.Core.Network;
 using Newtonsoft.Json;
@@ -22,9 +24,15 @@ namespace bioscoop_app.Controller
         }
 
 
+
+
+
+
+
         [HttpPost(Route = "/movies/add")]
         public ChromelyResponse AddMovie(ChromelyRequest request)
         {
+<<<<<<< Updated upstream
             var response = new Helper.Response { Status = 204, Data = "Hoi", StatusText = "OKOK" };
             var data = (JObject)JsonConvert.DeserializeObject(request.PostData.ToJson());
 
@@ -39,6 +47,46 @@ namespace bioscoop_app.Controller
                 context.SaveChanges();
             }
 
+=======
+
+
+            Dictionary<string, string> errorMessage = new Dictionary<string, string>();
+
+            if (String.IsNullOrWhiteSpace(data["title"].Value<string>()))
+            {
+
+                errorMessage.Add("title", "Er zit niks in title");
+            }
+            if (String.IsNullOrWhiteSpace(data["genre"].Value<string>()))
+            {
+                errorMessage.Add("genre", "Er zit niks in de genre");
+            }
+
+            if (String.IsNullOrWhiteSpace(data["rating"].Value<string>()))
+            {
+                errorMessage.Add("rating", "Er zit niks in de rating");
+            }
+
+            if (String.IsNullOrWhiteSpace(data["duration"].Value<string>()))
+            {
+                errorMessage.Add("duration", "Er zit niks in de duration");
+            }
+
+            var data = (JObject) JsonConvert.DeserializeObject(request.PostData.ToJson());
+            var movieRepository = new MovieRepository();
+            //get base64 image string
+            
+
+            movieRepository.Add(new Movie(
+                data["title"].Value<string>(),
+                data["genre"].Value<string>(),
+                data["rating"].Value<double>(),
+                data["duration"].Value<int>()
+            ));
+            
+            movieRepository.SaveChanges();
+
+>>>>>>> Stashed changes
             return new ChromelyResponse(request.Id)
             {
                 Data = response
