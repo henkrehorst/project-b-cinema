4c9da7fd81da3b7a7882b0e2811a6d97c2649eb7
Alexander <alexandervvugt@gmail.com>
2020-04-07 23:34:15 +0200

    Updated Room and ScreenTime

--- src/bioscoop_app/Model/ScreenTime.cs
+++ src/bioscoop_app/Model/ScreenTime.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace bioscoop_app.Model
@@ -9,23 +10,41 @@ namespace bioscoop_app.Model
 		public DateTime startTime;
 		public DateTime endTime;
 		public Room room;
+		public List<List<bool>> availability;
 
-		public ScreenTime(Movie movie, DateTime startTime, DateTime endTime /*, Room room*/)
+		public ScreenTime(Movie movie, DateTime startTime, DateTime endTime, Room room, List<List<bool>> availability)
 		{
 			this.movie = movie;
 			this.startTime = startTime;
 			this.endTime = endTime;
-			// this.room = room;
+			this.room = room;
+			this.availability = (availability is null) ? AvailabilityMap(room.layout) : availability;
 		}
 
 		[JsonConstructor]
-		public ScreenTime(int id, Movie movie, DateTime startTime, DateTime endTime /*, Room room*/)
+		public ScreenTime(int id, Movie movie, DateTime startTime, DateTime endTime, Room room, List<List<bool>> availability)
 		{
 			this.id = id;
 			this.movie = movie;
 			this.startTime = startTime;
 			this.endTime = endTime;
-			// this.room = room;
+			this.room = room;
+			this.availability = (availability is null) ? AvailabilityMap(room.layout) : availability;
+		}
+
+		private List<List<bool>> AvailabilityMap(List<List<Seat>> layout)
+		{
+			List<List<bool>> availability = new List<List<bool>>();
+			foreach (List<Seat> row in layout)
+			{
+				List<bool> boolrow = new List<bool>();
+				foreach (Seat seat in row)
+				{
+					boolrow.Add(seat is null);
+				}
+				availability.Add(boolrow);
+			}
+			return availability;
 		}
 	}
 
