4f8f37e1490e22ebda7d8ccb32d2bdff220fe5be
Alexander <alexandervvugt@gmail.com>
2020-04-23 14:53:55 +0200

    Created method to generate chromelyresponse from response

--- src/bioscoop_app/Helper/Response.cs
+++ src/bioscoop_app/Helper/Response.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Chromely.Core.Network;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +8,30 @@ namespace bioscoop_app.Helper
 {
     class Response
     {
-        public int Status { get; set; }
-        public string StatusText { get; set; }
-        public string Data { get; set; }
+        /// <summary>
+        /// The Http Status
+        /// </summary>
+        public int status;
+        /// <summary>
+        /// The message associated with the Status
+        /// </summary>
+        public string statusText;
+        /// <summary>
+        /// The data in json string.
+        /// </summary>
+        public string data;
+
+        /// <summary>
+        /// Converts current instance to a ChromelyResponse.
+        /// </summary>
+        /// <param name="reqId">The request Id</param>
+        /// <returns>This as a ChromelyResponse</returns>
+        public ChromelyResponse ChromelyWrapper(string reqId)
+        {
+            return new ChromelyResponse(reqId)
+            {
+                Data = JsonConvert.SerializeObject(this)
+            };
+        }
     }
 }
