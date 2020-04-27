6515265fb9260e0d1393bceb8a594d97269275ac
henkrehorst <henkrehorst@outlook.com>
2020-04-27 19:45:27 +0200

    fix last bugs screentime javascript

--- src/bioscoop_app/frontend/admin/js/screentime_pages.js
+++ src/bioscoop_app/frontend/admin/js/screentime_pages.js
@@ -4,14 +4,16 @@
  */
 async function screentimeOverviewPage() {
     // get all screentimes
-    const screenTimes = await chromelyRequest('/screentime');
+    const screenTimesResponse = await chromelyRequest('/screentime');
+    let screenTimes = screenTimesResponse.getData();
     let screenTimeTable = "";
     
     // create a table with screentimes
     for(key in screenTimes){
         //get movie by screentime
-        let movie = await chromelyRequest('/movies#id','POST',{'id': screenTimes[key].movie});
-        
+        let movieResponse = await chromelyRequest('/movies#id','POST',{'id': screenTimes[key].movie});
+        let movie = movieResponse.getData();
+            
         screenTimeTable += `<tr>
                 <td>${movie.title}</td>
                 <td>${new Date(screenTimes[key].startTime).toLocaleString()}</td>
@@ -32,30 +34,38 @@ async function screentimeEditPage() {
     //prepare movie dropdown
     await fillMovieDropdown();
     //get screen time by id
-    let screenTime = await chromelyRequest('/screentime#id','POST',{'id': getIdFromUrl()})
+    let screenTimeResponse = await chromelyRequest('/screentime#id','POST',{'id': getIdFromUrl()});
+    let screenTime = screenTimeResponse.getData();
     
-    //file movie form
+    //make current item active in movie dropdown
     let movieDropdown = document.querySelector("#movies_field");
-    movieDropdown.options[screenTime.movie].selected = true;
+    for(key in movieDropdown.options){
+        if(movieDropdown.options[key].value === screenTime.movie.toString()){
+            movieDropdown.options[key].selected = true;
+        }
+    }
+    
     document.querySelector("#start_time").value = screenTime.startTime;
     document.querySelector("#end_time").value = screenTime.endTime;
 
     /**
      * function for updating screentime in backend
      */
-    function updateScreenTime() {
+    async function updateScreenTime() {
         // get screentime form data
         const screenTimeForm = new FormData(document.querySelector("body > div > div > div > form"));
 
         // post screentime to backend
-        chromelyRequest('/screentime#update', 'POST', {
+        const response = await chromelyRequest('/screentime#update', 'POST', {
             'id': getIdFromUrl(),
             'movie_id': screenTimeForm.get('movie'),
             'start_time': screenTimeForm.get('start_time'),
             'end_time': screenTimeForm.get('end_time')
-        }).then(value => {
-            return window.location.href = "/admin/screentime.html";
         })
+        
+        if(response.getStatusCode() === 204){
+            window.location.href = "/admin/screentime.html";
+        }
     }
 
     document.querySelector("body > div > div > div > form > div:nth-child(4) > button").addEventListener('click', updateScreenTime);
@@ -72,18 +82,20 @@ async function screentimeAddPage() {
     /**
      * function for posting screentime to backend
      */
-    function addScreenTime() {
+    async function addScreenTime() {
         // get screentime form data
         const screenTimeForm = new FormData(document.querySelector("body > div > div > div > form"));
         
         // post screentime to backend
-        chromelyRequest('/screentime/add', 'POST', {
+        const response = await chromelyRequest('/screentime/add', 'POST', {
             'movie_id': screenTimeForm.get('movie'),
             'start_time': screenTimeForm.get('start_time'),
             'end_time': screenTimeForm.get('end_time')
-        }).then(value => {
-            return window.location.href = "/admin/screentime.html";
         })
+        
+        if(response.getStatusCode() === 204){
+            window.location.href = "/admin/screentime.html";
+        }
     }
     
     document.querySelector("body > div > div > div > form > div:nth-child(4) > button").addEventListener('click', addScreenTime);
@@ -95,7 +107,8 @@ async function screentimeAddPage() {
  * @returns {Promise<void>}
  */
 async function fillMovieDropdown() {
-    let movies = await chromelyRequest('/movies');
+    let moviesResponse = await chromelyRequest('/movies');
+    let movies = moviesResponse.getData();
     for(let movie in movies){
         document.querySelector("#movies_field").innerHTML += 
             `<option value="${movies[movie].Id}">${movies[movie].title}</option>`;
