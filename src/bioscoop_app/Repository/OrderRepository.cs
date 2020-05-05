8cd73f35245aaf86349f99e19da631015f090c50
Alexander <alexandervvugt@gmail.com>
2020-05-05 12:06:55 +0200

    Create OrderRepository

---
+++ src/bioscoop_app/Repository/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using bioscoop_app.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bioscoop_app.Repository
+{
+    class OrderRepository : Repository<Order>
+    {
+    }
+}
