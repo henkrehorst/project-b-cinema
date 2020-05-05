d5a21652be9e262f640d4b68247aab35b39b20bc
Alexander <alexandervvugt@gmail.com>
2020-05-05 12:08:21 +0200

    added docstring

--- src/bioscoop_app/Repository/OrderRepository.cs
+++ src/bioscoop_app/Repository/OrderRepository.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace bioscoop_app.Repository
 {
+    /// <summary>
+    /// Repository class that handles data storage for Order type.
+    /// </summary>
     class OrderRepository : Repository<Order>
     {
     }
