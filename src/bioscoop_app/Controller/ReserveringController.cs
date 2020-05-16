0066cf7010556392fd3d93cfafc5c22b1c3c2466
ercu5314 <ercument-58@outlook.com>
2020-05-16 22:57:35 +0200

    Forms toegevoegd en de c# klaargezet
    Hierbij zijn de forms toegevoegd en de gewenste c# bestanden. Ze moeten nog werkend zien te krijgen

---
+++ src/bioscoop_app/Controller/ReserveringController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using bioscoop_app.Helper;
+using bioscoop_app.Model;
+using bioscoop_app.Repository;
+using bioscoop_app.Service;
+using Chromely.Core.Network;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace bioscoop_app.Controller
+{
+    /// <summary>
+    /// Class that controls the routes related to Movie.
+    /// </summary>
+    public class MovieController : ChromelyController
+    {
+        /// <param name="request">http GET request</param>
+        /// <returns>All Movies in the data file.</returns>
+        [HttpGet(Route = "/movies")]
+        public ChromelyResponse GetMovies(ChromelyRequest request)
+        {
+            var movieRepository = new MovieRepository();
+
+            var movies = movieRepository.Data;
+
+            return new Response
+            {
+                status = 200,
+                data = JsonConvert.SerializeObject(movies)
+            }.ChromelyWrapper(request.Id);
+        }
+
+        /// <param name="req">http POST request containing the desired Movie's id</param>
+        /// <returns>The Movie associated with the id.</returns>
+        [HttpPost(Route = "/movies#id")]
+        public ChromelyResponse GetMovieById(ChromelyRequest req)
+        {
+            int id = ((JObject)JsonConvert.DeserializeObject(req.PostData.ToJson())).Value<int>("id");
+            return new Response
+            {
+                status = 200,
+                data = JsonConvert.SerializeObject(new MovieRepository().Data[id])
+            }.ChromelyWrapper(req.Id);
+        }
+
+        /// <summary>
+        /// Adds a movie to the data file.
+        /// </summary>
+        /// <param name="request">http POST request containing the movie</param>
+        /// <returns>Status 204 indicating the movie was added successfully</returns>
+        [HttpPost(Route = "/movies/add")]
+        public ChromelyResponse AddMovie(ChromelyRequest request)
+        {
+            var data = (JObject) JsonConvert.DeserializeObject(request.PostData.ToJson());
+            var movieRepository = new MovieRepository();
+            string fileName = "";
+            
+            //get base64 image string
+            
+
+
+            movieRepository.Add(new Movie(
+                data["naam"].Value<string>(),
+                data["email"].Value<string>(),
+                data["producten"].Value<double>(),
+                fileName
+            ));
+            
+            movieRepository.SaveChangesThenDiscard();
+
+            return new Response
+            {
+                status = 204
+            }.ChromelyWrapper(request.Id);
+        }
+
+        /// <summary>
+        /// Updates the movie associated with the specified id, with the specified data.
+        /// </summary>
+        /// <param name="req">http POST request containing the id and data</param>
+        /// <returns>Status code indicating success or failure.</returns>
+        [HttpPost(Route = "/movies#update")]
+        public ChromelyResponse UpdateMovie(ChromelyRequest req)
+        {
+            JObject data = (JObject)JsonConvert.DeserializeObject(req.PostData.ToJson());
+            Repository<Movie> repository = new MovieRepository();
+            
+            string filestring = data["cover_image"].Value<string>();
+            string filename = repository.Data[data.Value<int>("id")].coverImage;
+            
+            if (filestring.Length > 0)
+            {
+                var uploadService = new UploadService(filestring);
+                if (uploadService.CheckIsImage())
+                {
+                    uploadService.DeleteFile(filename);
+                    uploadService.CreateFileInUploadFolder();
+                    filename = uploadService.GetFileName();
+                }
+            }
+            
+            try
+            {
+                repository.Update(data.Value<int>("id"), new Movie(
+                    data["title"].Value<string>(),
+                    data["genre"].Value<string>(),
+                    data["rating"].Value<double>(),
+                    data["samenvatting"].Value<string>(),
+                    data["duration"].Value<int>(),
+                    filename
+                ));
+            } catch(InvalidOperationException except)
+            {
+                return new Response
+                {
+                    status = 400,
+                    statusText = except.Message
+                }.ChromelyWrapper(req.Id);
+            }
+            repository.SaveChangesThenDiscard();
+            return new Response
+            {
+                status = 200,
+                data = req.PostData.ToJson()
+            }.ChromelyWrapper(req.Id);
+        }
+    }
+}
---
+++ src/bioscoop_app/frontend/medewerkers/js/reservering.js
@@ -0,0 +1,127 @@
+﻿/**
+ * function with all javascript running on the movie overview admin page
+ * @returns {Promise<void>}
+ */
+async function moviesOverviewPage() {
+    // get all movies
+    const moviesResponse = await chromelyRequest('/movies');
+    console.log(moviesResponse);
+    let movies = moviesResponse.getData();
+    let movieTable = "";
+
+    // create a table with screentimes
+    for(movie in movies){
+        movieTable += `<tr>
+                <td>${movies[movie].naam}</td>
+                <td>${movies[movie].email}</td>
+                <td>${movies[movie].producten}</td>
+                <td><a href='/medewerkers/reservering_edit.html?id=${movies[movie].Id}'>Edit</a></td>
+            </tr>`;
+    }
+
+    //display table
+    document.querySelector("body > div > div > div > table > tbody").innerHTML += movieTable;
+}
+
+/**
+ * function with all javascript running on the movie edit page
+ * @returns {Promise<void>}
+ */
+async function movieEditPage() {
+    //get movie by id
+    const movieResponse = await chromelyRequest('/movies#id','POST',{'id': getIdFromUrl()});
+    let movie = movieResponse.getData();
+    
+    //fill form with movie data
+    document.getElementById('naam').value = movie.naam;
+    document.getElementById('email').value = movie.email;
+    document.getElementById('producten').value = movie.producten;
+    
+    /**
+     * function for posting movie to backend
+     */
+    async function updateMovie() {
+        // get movie form data
+        const movieForm = new FormData(document.querySelector("body > div > div > div > form"));
+       
+
+        // post movie to backend
+        const response = await chromelyRequest('/movies#update', 'POST', {
+            'id': getIdFromUrl(),
+            'naam': movieForm.get('naam'),
+            'email': movieForm.get('email'),
+            'producten': movieForm.get('producten'),
+            
+        })
+
+        // if response = 200: redirect to movie overview page
+        if(response.getStatusCode() === 200){
+            window.location.href = "/medewerkers/reservering.html"
+        }
+    }
+
+    document.querySelector("body > div > div > div > form > div > button").addEventListener('click', updateMovie);
+}
+
+/**
+ * function with all javascript running on the movie add page
+ * @returns {Promise<void>}
+ */
+async function reserveringAddPage() {
+    /**
+     * function for posting movie to backend
+     */
+    async function addMovie() {
+        // get movie form data
+        const movieForm = new FormData(document.querySelector("body > div > div > div > form"));
+        
+            
+        // post movie to backend
+        const response = await chromelyRequest('/movies/add', 'POST', {
+                'naam': movieForm.get('naam'),
+                'email': movieForm.get('email'),
+                'producten': movieForm.get('producten'),
+               
+            })
+            
+            // if response = 204: redirect to movie overview page
+            if(response.getStatusCode() === 204){
+                window.location.href = "/medewerkers/reservering.html"
+            }
+    }
+
+    document.querySelector("body > div > div > div > form > div > button").addEventListener('click', addMovie);
+}
+
+/**
+ * Convert file to base 64 string
+ * @param file
+ * @returns {Promise<string>}
+ */
+async function getBase64String(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            if(reader.result === "data:"){
+                resolve("")
+            }else {
+                resolve(reader.result);
+            }
+        };
+        reader.error = function (error) {
+            reject(error);
+        };
+    }).then(value => value)
+}
+
+/**
+ * show a preview of the uploaded cover image
+ * @returns {Promise<void>}
+ */
+async function coverImagePreview() {
+    // get movie form data
+    const movieForm = new FormData(document.querySelector("body > div > div > div > form"));
+   
+   
+}
