8d50712068f47e4577df82c596f00a998a44a9b7
henkrehorst <henkrehorst@outlook.com>
2020-05-18 14:38:56 +0200

    add security model frontend

--- src/bioscoop_app/frontend/js/main.js
+++ src/bioscoop_app/frontend/js/main.js
@@ -1,4 +1,70 @@
 ﻿/**
+ * enable security model
+ */
+function addSecurityModel() {
+
+
+    //add security model in body
+    document.querySelector("body").innerHTML +=
+        `<div id="security-model" class="modal">
+            <div class="modal-content">
+                <span id="clossButton" class="close">&times;</span>
+                <h1>Inloggen medewerker/beheerder</h1>
+                <p>Voer hieronder je gegevens in, om in te loggen.</p>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                        <form id="securityForm">
+                            <div class="form-group mt-2">
+                                <label for="password-field text-left">Wachtwoord:</label>
+                                <input name="password" type="password" placeholder="Wachtwoord" class="form-control" id="password-field">
+                            </div>
+                            <div id="error-message"></div>
+                            <div class="form-group">
+                                <button type="button" onclick="userLogin()" id="signInButton" class="btn btn-primary btn-block">INLOGGEN</button>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>`;
+
+    const securityModel = document.getElementById('security-model');
+    const closeButton = document.getElementById('clossButton');
+
+    //if user click outside the security model, close the model
+    window.onclick = function (event) {
+        if (event.target === securityModel) {
+            securityModel.style.display = "none";
+            document.querySelector('body').style.overflowY = "auto"
+        }
+    }
+
+    //close model by onclick close button
+    closeButton.onclick = () => {
+        securityModel.style.display = "none";
+        document.querySelector('body').style.overflowY = "auto"
+    }
+
+    function showSecurityModel() {
+        document.getElementById("security-model").style.display = "block";
+        document.querySelector('body').style.overflowY = "hidden"
+    }
+
+    //show security model when short cut is pressed
+    document.onkeydown = (event) => {
+        if (event.ctrlKey && event.shiftKey) {
+            //show model
+            showSecurityModel();
+        }
+    }
+}
+
+addSecurityModel()
+
+/**
  * load dynamic items in navbar
  */
 function fillNavbar() {
@@ -80,7 +146,7 @@ class chromelyResponse {
         //convert json data string to object
         try {
             this.data = JSON.parse(data)
-        }catch (e) {
+        } catch (e) {
             this.data = "";
         }
     }
@@ -88,21 +154,55 @@ class chromelyResponse {
     /**
      * @returns number
      */
-    getStatusCode(){
+    getStatusCode() {
         return this.status;
     }
 
     /**
      * @returns {*}
      */
-    getData(){
+    getData() {
         return this.data;
     }
 
     /**
      * @returns string
      */
-    getStatusText(){
+    getStatusText() {
         return this.statusText;
+    }
+}
+
+/**
+ * check user login in backend
+ */
+async function userLogin() {
+    //get password from security form
+    let password = new FormData(document.getElementById('securityForm')).get('password');
+
+    //check password in backend
+    const response = await chromelyRequest('/security/login', 'POST', {password: password})
+    
+    //if status code  === 200, password is correct redirect user to correct screen
+    if (response.getStatusCode() === 200) {
+        if (response.getData().role === "cashier") {
+            //redirect to cashier screen
+            window.location.href = "./medewerkers/medewerker.html";
+        } else if (response.getData().role === "admin") {
+            //redirect to admin screen
+            window.location.href = "./admin/admin.html";
+        } else {
+            //show error message
+            document.getElementById('error-message').innerHTML =
+            `<div class="alert alert-primary" role="alert">
+                  Er is iets fout gegaan probeer het nog een keer!
+            </div>`;
+        }
+    } else {
+        //show error message if password is incorrect
+        document.getElementById('error-message').innerHTML =
+            `<div class="alert alert-danger" role="alert">
+                  Incorrect wachtwoord, probeer het nog een keer!
+            </div>`;
     }
 }
