8a3bfef672de9bd3ee66fe6cf37128f4e83af40a
Alexander <alexandervvugt@gmail.com>
2020-05-18 22:04:39 +0200

    Implemented frontend get reservation by code.

--- src/bioscoop_app/frontend/js/order.js
+++ src/bioscoop_app/frontend/js/order.js
@@ -1,1 +1,16 @@
-﻿
+﻿document.getElementById("getorder").addEventListener("click", () => {
+    let code = document.getElementById("code-field").value;
+    let res = await chromelyRequest("/order#fetch", "POST", { "code": code });
+    if (res.getStatusCode == 200) {
+        displayOrderState(res.getData());
+    } else if (res.getStatusCode == 204) {
+        //Display error message
+        Console.log(res.getStatusText());
+    } else {
+        Console.log(res.getStatusCode(), res.getStatusText());
+    }
+})
+
+function displayOrderState(data) {
+    //display order
+}
