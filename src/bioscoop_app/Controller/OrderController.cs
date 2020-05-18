a950bf265c84c03e518d9565e70750c9704a06ec
Alexander <alexandervvugt@gmail.com>
2020-05-18 19:00:05 +0200

    Implemented order updates
    refactored some functionality in OrderController to be more dynamic, so it can also be used when updating and cancelling reservations.

--- src/bioscoop_app/Controller/OrderController.cs
+++ src/bioscoop_app/Controller/OrderController.cs
@@ -6,7 +6,10 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace bioscoop_app.Controller
@@ -16,6 +19,12 @@ namespace bioscoop_app.Controller
     /// </summary>
     class OrderController : ChromelyController
     {
+        private Func<List<Product>, List<Ticket>> filterTickets = sequence =>
+        {
+            return (from product in sequence
+                    where product.GetType() == typeof(Ticket)
+                    select (Ticket)product).ToList();
+        };
         /// <summary>
         /// Searches the order with the specified code in the data, and returns it.
         /// </summary>
@@ -59,21 +68,13 @@ namespace bioscoop_app.Controller
         public ChromelyResponse CreateOrder(ChromelyRequest req)
         {
             var data = (JObject)JsonConvert.DeserializeObject(req.PostData.ToJson());
-
-            //parse items
-            var prods = data["items"].Value<JArray>();
-            List<Product> products = new List<Product>();
-            foreach(JObject product in prods)
-            {
-                products.Append(ProductController.ToProduct(product));
-            }
-
+            List<Product> products = ParseItems(data["items"].Value<JArray>());
             Order order = new Order(
                     products,
                     data["cust_name"].Value<string>(),
                     data["cust_email"].Value<string>()
                 );
-            ReserveTickets(order.items);
+            SetSeatsAvailability(filterTickets(order.items), true);
             new Repository<Order>().AddThenWrite(order);
             return new Response
             {
@@ -82,16 +83,89 @@ namespace bioscoop_app.Controller
             }.ChromelyWrapper(req.Id);
         }
 
-        private void ReserveTickets(List<Product> items)
+        /// <summary>
+        /// Updates the order associated with the specified id, with the specified data.
+        /// </summary>
+        /// <param name="req">http POST request containing the id and data</param>
+        /// <returns>Status code indicating success or failure.</returns>
+        [HttpPost(Route = "/reserveringen#update")]
+        public ChromelyResponse UpdateOrder(ChromelyRequest req)
         {
-            var tickets = items.Where(p => p.GetType() == typeof(Ticket))
-                .Select(t => t);
-            foreach(Ticket ticket in tickets)
+            //throw new NotImplementedException();
+            JObject data = (JObject)JsonConvert.DeserializeObject(req.PostData.ToJson());
+            Repository<Order> repository = new Repository<Order>();
+            try
             {
-                var repo = new Repository<ScreenTime>();
-                repo.Data[ticket.screenTime].ReserveSeat(ticket);
-                repo.SaveChangesThenDiscard();
+                int orderId = data["id"].Value<int>();
+                Order input = new Order(
+                        orderId,
+                        ParseItems(data["items"].Value<JArray>()),
+                        data["code"].Value<string>(),
+                        data["cust_name"].Value<string>(),
+                        data["cust_email"].Value<string>()
+                    );
+                if (input.items.SequenceEqual(repository.Data[orderId].items))
+                {
+                    //Backend magic to change availability of items
+                    List<Ticket> inputTickets = filterTickets(input.items);
+                    List<Ticket> existingTickets = filterTickets(repository.Data[orderId].items);
+                    if (inputTickets.Any() && !existingTickets.Any())
+                    {
+                        //Reserve tickets
+                        SetSeatsAvailability(inputTickets, false);
+                    }
+                    if (!inputTickets.Any() && existingTickets.Any())
+                    {
+                        // Cancel ticket reservation
+                        SetSeatsAvailability(existingTickets, true);
+                    }
+                    if (inputTickets.Any() && existingTickets.Any() && !inputTickets.SequenceEqual(existingTickets))
+                    {
+                        //fix ticket difference in data
+                        List<Ticket> reserve = inputTickets.Except(existingTickets).ToList(); //A - B
+                        List<Ticket> cancel = existingTickets.Except(inputTickets).ToList(); // B - A
+                        SetSeatsAvailability(reserve, true);
+                        SetSeatsAvailability(cancel, false);
+                    }
+                }
+                repository.Update(
+                        orderId,
+                        input
+                    );
             }
+            catch (InvalidOperationException except)
+            {
+                return new Response
+                {
+                    status = 400,
+                    statusText = except.Message
+                }.ChromelyWrapper(req.Id);
+            }
+            repository.SaveChangesThenDiscard();
+            return new Response
+            {
+                status = 200,
+            }.ChromelyWrapper(req.Id);
+        }
+
+        private void SetSeatsAvailability(List<Ticket> tickets, bool value)
+        {
+            Repository<ScreenTime> repo = new Repository<ScreenTime>();
+            foreach (Ticket ticket in tickets)
+            {
+                repo.Data[ticket.screenTime].SetSeatAvailability(ticket, value);
+            }
+            repo.SaveChangesThenDiscard();
+        }
+
+        private List<Product> ParseItems(JArray dataSeq)
+        {
+            List<Product> products = new List<Product>();
+            foreach (JObject product in dataSeq)
+            {
+                products.Append(ProductController.ToProduct(product));
+            }
+            return products;
         }
     }
 }
