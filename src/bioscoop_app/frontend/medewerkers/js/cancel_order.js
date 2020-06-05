fe292224bf925deb5c9dc0f13641aa970b3e6928
Alexander <alexandervvugt@gmail.com>
2020-06-05 15:20:46 +0200

    cancel order functionality for employee

---
+++ src/bioscoop_app/frontend/medewerkers/js/cancel_order.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Eventlistener to cancel the order by the input code.
+ */
+document.getElementById("cancel").addEventListener("click", async () => {
+    let code = document.getElementById("code-field").value;
+    let res = await chromelyRequest("/order#cancel", "POST", { "code": code });
+    if (res.getStatusCode() === 200) {
+        console.log("cancel order 200");
+    } else if (res.getStatusCode() === 204) {
+        //Display error message
+        console.log(res.getStatusText());
+    } else {
+        console.log(res.getStatusCode(), res.getStatusText());
+    }
+});
