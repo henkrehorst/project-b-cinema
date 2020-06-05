e49b92ec3357002d33fb156d5775f2f2c37877c0
henkrehorst <henkrehorst@outlook.com>
2020-06-05 15:25:24 +0200

    added error when no tickets selected in order flow step 1

--- src/bioscoop_app/frontend/js/main.js
+++ src/bioscoop_app/frontend/js/main.js
@@ -225,3 +225,16 @@ async function userLogin() {
             </div>`;
     }
 }
+
+/**
+ * calculate size of javascript object
+ * @param obj
+ * @return {number}
+ */
+Object.size = function(obj) {
+    let size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
