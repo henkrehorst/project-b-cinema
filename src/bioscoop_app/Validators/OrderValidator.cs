cc684f86f4a7fd7d12058e7956ad5ab228f9a604
Alexander <alexandervvugt@gmail.com>
2020-06-07 21:49:37 +0200

    Created ordervalidator, incomplete

---
+++ src/bioscoop_app/Validators/OrderValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using bioscoop_app.Model;
+using FluentValidation;
+
+namespace bioscoop_app.Validators
+{
+    class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(order => order.cust_name).NotNull().NotEmpty();
+            RuleFor(order => order.cust_email).EmailAddress();
+        }
+    }
+}
