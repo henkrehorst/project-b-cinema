e2dc56002f7d27faed1c94eb9c9b075bb5d828db
jordyegh <jordyegh@gmail.com>
2020-06-30 20:43:40 +0200

    Gift voucher form added

--- src/bioscoop_app/frontend/js/gift_purchase.js
+++ src/bioscoop_app/frontend/js/gift_purchase.js
@@ -1,25 +1,18 @@
 ﻿function loadNav() {
-    document.querySelector("nav > div").innerHTML =
+    document.querySelector('nav > div').innerHTML =
         `<ul>
            <li><a href=\"/index.html\">De kijkdoos</a></li>
         </ul>
         <ul>
-          <li class=\"menu-item\"><a href="./gift_purchase.html">Gift Kopen</a></li>
+          <li class=\"menu-item active\"><a href="./gift_purchase.html">Kado Kopen</a></li>
           <li class=\"menu-item\"><a href="./index.html">Films</a></li>
           <li class=\"menu-item\"><a href="./order.html">Reserveringen</a></li>
         </ul>`;
-
-    let elNav = document.querySelector("body > nav");
-
-    if (elNav.dataset.activeItem !== undefined) {
-        let navItems = document.getElementsByClassName("menu-item");
-
-        for (let i = 0; i < navItems.length; i++) {
-            if (navItems[i].innerText == elNav.dataset.activeItem) {
-                navItems[i].classList.add("active");
-            }
-        }
-    }
 }
 
 loadNav();
+
+function submitForm() {
+    let formData = new FormData(document.getElementById('gift-form'));
+    console.log(formData.get('name'), formData.get('email'))
+}
