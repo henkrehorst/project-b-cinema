85f5f2045916326ad54a4ed64dea8ab924cef4cc
jordyegh <jordyegh@gmail.com>
2020-06-30 19:36:28 +0200

    Navigation loading and highlighting for gift purchase page

---
+++ src/bioscoop_app/frontend/js/gift_purchase.js
@@ -0,0 +1,25 @@
+﻿function loadNav() {
+    document.querySelector("nav > div").innerHTML =
+        `<ul>
+           <li><a href=\"/index.html\">De kijkdoos</a></li>
+        </ul>
+        <ul>
+          <li class=\"menu-item\"><a href="./gift_purchase.html">Gift Kopen</a></li>
+          <li class=\"menu-item\"><a href="./index.html">Films</a></li>
+          <li class=\"menu-item\"><a href="./order.html">Reserveringen</a></li>
+        </ul>`;
+
+    let elNav = document.querySelector("body > nav");
+
+    if (elNav.dataset.activeItem !== undefined) {
+        let navItems = document.getElementsByClassName("menu-item");
+
+        for (let i = 0; i < navItems.length; i++) {
+            if (navItems[i].innerText == elNav.dataset.activeItem) {
+                navItems[i].classList.add("active");
+            }
+        }
+    }
+}
+
+loadNav();
