7c748aec1590ea4d1e425e453d399681407b219f
jordyegh <jordyegh@gmail.com>
2020-07-10 14:01:56 +0200

    Added asynchronous chromelyRequest function for post data handling

--- src/bioscoop_app/frontend/js/gift_purchase.js
+++ src/bioscoop_app/frontend/js/gift_purchase.js
@@ -1,4 +1,24 @@
-﻿function loadNav() {
+﻿async function chromelyRequest(route, method = 'GET', postData = {}) {
+    return new Promise((resolve, reject) => {
+        var request = {
+            'method': method,
+            'url': route,
+            'parameters': null,
+            'postData': postData
+        };
+        window.cefQuery({
+            request: JSON.stringify(request),
+            onSuccess: function (response) {
+                let result = JSON.parse(JSON.parse(response).Data);
+                resolve(new chromelyResponse(result.status, result.statusText, result.data))
+            }, onFailure: function (err, msg) {
+                reject(err)
+            }
+        });
+    }).then(data => data);
+}
+
+function loadNav() {
     document.querySelector('nav > div').innerHTML =
         `<ul>
            <li><a href=\"/index.html\">De kijkdoos</a></li>
@@ -14,9 +34,9 @@ loadNav();
 
 function submitForm() {
     let formData = new FormData(document.getElementById('gift-form'));
-    console.log(formData.get('name'), formData.get('email'))
+    console.log(formData.get('name'), formData.get('email'));
 
-    res = chromelyRequest('/gift#create', 'POST', order);
+    res = chromelyRequest('/gift#create', 'POST', formData);
     console.log(res.getData());
 }
 
